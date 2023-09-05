f8d8483d3448a2a0d3b65459dda71b571c6a7e08
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-05 22:20:51 +0530

    💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓🎩

--- commands/_help.js
+++ commands/_help.js
@@ -1,122 +1,4 @@
 /**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
-
-const os = require('os')
-const moment = require("moment-timezone")
-const fs = require("fs")
-const Config = require('../config')
-let { fancytext, tlang, tiny, runtime, formatp, botpic, prefix, sck1 } = require("../lib");
-const long = String.fromCharCode(8206)
-const readmore = long.repeat(4001)
-const Secktor = require('../lib/commands')
-
-    //---------------------------------------------------------------------------
-Secktor.cmd({
-            pattern: "help",
-            alias: ["menu"],
-            desc: "Help list",
-            category: "general",
-            react: "🧚‍♂️",
-            filename: __filename
-        },
-        async(Void, citel, text) => {
-            const { commands } = require('../lib');
-            if (text.split(" ")[0]) {
-                let arr = [];
-                const cmd = commands.find((cmd) => cmd.pattern === (text.split(" ")[0].toLowerCase()))
-                if (!cmd) return await citel.reply("*❌No Such commands.*");
-                else arr.push(`*🍁Command:* ${cmd.pattern}`);
-                if (cmd.category) arr.push(`*🧩Category:* ${cmd.category}`);
-                if (cmd.alias) arr.push(`*🧩Alias:* ${cmd.alias}`);
-                if (cmd.desc) arr.push(`*🧩Description:* ${cmd.desc}`);
-                if (cmd.use) arr.push(`*〽️Usage:*\n \`\`\`${prefix}${cmd.pattern} ${cmd.use}\`\`\``);
-                return await citel.reply(arr.join('\n'));
-            } else {
-                const cmds = {}
-                commands.map(async(command, index) => {
-                    if (command.dontAddCommandList === false && command.pattern !== undefined) {
-                        if (!cmds[command.category]) cmds[command.category] = []
-                        cmds[command.category].push(command.pattern)
-                    }
-                })
-                const time = moment(moment())
-                    .format('HH:mm:ss')
-                moment.tz.setDefault('Asia/KOLKATA')
-                    .locale('id')
-                const date = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
-                let total = await sck1.countDocuments()
-                let str = `🍁 ` + fancytext(Config.ownername.split(' ')[0], 58) + ` 🍁\n`
-                str +=
-                    '' + `𝗛𝗜 ${citel.pushName}
-╔══════❍
-║𝐃𝚫𝚪𝐊 𝐐𝐔𝚵𝚵𝚴
-╠══════════❍
-║ 𝐔𝐒𝐄𝐑:- ${citel.pushName}
-║ 𝐓𝐇𝐄𝐌𝐄𝐒:- ${tlang().title}
-║ 𝐏𝐑𝐄𝐅𝐈𝐗:-  ${prefix} 
-║ 𝐎𝐖𝐍𝐄𝐑:- ᴍʀ•ɴᴏᴛɪʏᴀ
-╚══════════❍
-╔═══════════════❍
-║𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐋𝐈𝐒𝐓
-╚═══════════════❍\n
-` + ''
-                for (const category in cmds) 
-                {
-                   str += `╔═❮ *${tiny(category)}*❯══❍\n` ;
-                   if(text.toLowerCase() == category.toLowerCase()){ str = `╔═❮ *${tiny(category)}*\n❯══❍` ;      
-                        for (const plugins of cmds[category]) { str += `║🍁⃞➢ ${fancytext(plugins,1)}\n` ; }
-                        str += `╚════════════❍\n`  ;
-                        break ;
-                   }
-                   else { for (const plugins of cmds[category]) { str += `║ 💕⃞➢ ${fancytext(plugins,1)}\n` ; }
-                         str += `╚══════════❍\n`  ; 
-                   }
-  
-                }
-                str+= `*ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ 
-ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴍᴅ*`
-                let buttonMessaged = {
-                    image: { url: await botpic() },
-                    caption: str
-                };
-                return await Void.sendMessage(citel.chat, buttonMessaged);
-            }
-        }
-    )
-    //---------------------------------------------------------------------------
-Secktor.cmd({
-            pattern: "cmd",
-            desc: "cmd1",
-            category: "general"
-        },
-        async(Void, citel) => {
-            const { commands } = require('../lib');
-            let str = `
-╭━━〘 ` + fancytext(Config.ownername.split(' ')[0], 58) + ` 〙━━──⊷`
-            str += `
-┃ ⛥╭──────────────      
-┃ ⛥│ User: ${citel.pushName}
-┃ ⛥│ Theme: ${tlang().title}
-┃ ⛥│ Prefix: ${prefix}
-┃ ⛥│ Owner: ${Config.ownername}
-┃ ⛥│ Commands: ${commands.length}
-/**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : DARK_QUEEN
- * @author : X-Notiya <https://github.com/X-Notiya>
- * @description : X-Notiya,A Multi-functional whatsapp bot.
- * @version 0.0.1
  🎭𝑫𝑨𝑹𝑲 𝑸𝑼𝑬𝑬𝑵 𝑴𝑫🎭
 🎩𝑪𝑹𝑬𝑨𝑻𝑬𝑫 𝑩𝒀 𝑪𝑯𝑨𝑴𝑶𝑫𝑯🎩
  **/
@@ -136,7 +18,7 @@ Secktor.cmd({
             alias: ["menu"],
             desc: "Help list",
             category: "general",
-            react: "🦂",
+            react: "💓",
             filename: __filename
         },
         async(Void, citel, text) => {
@@ -165,7 +47,7 @@ Secktor.cmd({
                     .locale('id')
                 const date = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
                 let total = await sck1.countDocuments()
-                let str = `🎭` + fancytext(Config.ownername.split('DARK QUEEN V1 ')[0], 24) + `\n`
+                let str = `❋` + fancytext(Config.ownername.split('DARK QUEEN ')[0], 23) + ` ❋\n`
                 str +=
                     '```' + `┏━━━━━●
 ┃```Hellow ${citel.pushName}```
@@ -179,7 +61,7 @@ Secktor.cmd({
 ┃ 💓𝐷𝐴𝑇𝐸●➢${date}
 ┗━━━━━━━━━━●\n
 ` + '```'
-                for (const category in cmds) 
+                 for (const category in cmds) 
                 {
                    str += `+●━━🧜‍♀️${tiny(category)}🧜‍♀️━━●\n` ;
                    if(text.toLowerCase() == category.toLowerCase()){ str = `●━━🧚‍♂️${tiny(category)}🧚‍♂️━━●\n` ;      
@@ -239,7 +121,7 @@ Secktor.cmd({
         pattern: "owner",
         desc: "To find owner number",
         category: "general",
-        react: "🎩",
+        react: "💜",
         filename: __filename
     },
     async(Void, citel) => {
@@ -255,8 +137,7 @@ Secktor.cmd({
             contextInfo: {
                 externalAdReply: {
                     title: Config.ownername,
-                    body: '🎭𝑫𝑨𝑹𝑲 𝑸𝑼𝑬𝑬𝑵 𝑴𝑫🎭
-🎩𝑪𝑹𝑬𝑨𝑻𝑬𝑫 𝑩𝒀 𝑪𝑯𝑨𝑴𝑶𝑫𝑯🎩',
+                    body: 'Touch here.',
                     renderLargerThumbnail: true,
                     thumbnailUrl: ``,
                     thumbnail: log0,
