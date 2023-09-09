64ba77c3bcf3276880b0a21ec2d076011f0efb64
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-09 13:48:40 +0530

    💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓

--- commands/general.js
+++ commands/general.js
@@ -9,57 +9,25 @@
  * @version 0.0.6
  **/
 
-const { tlang, botpic,cmd, prefix, runtime,Config } = require('../lib')
+const { tlang, botpic, cmd, prefix, runtime, Config , sleep } = require('../lib')
 const axios = require('axios')
 const speed = require('performance-now')
+const fetch = require('node-fetch');
 //---------------------------------------------------------------------------
+
 cmd({
-        pattern: "chat",
-        desc: "chat with an AI",
-        category: "general",
-        use: '<Hii,Secktor>',
-        filename: __filename,
-    },
-    async(Void, citel,text) => {
-        let zx = text.length;
-        if (zx < 8) {
-            let {data} = await axios.get(`http://api.brainshop.ai/get?bid=167991&key=aozpOoNOy3dfLgmB&uid=[${citel.sender.split("@")[0]}]&msg=[${text}]`);
-            return citel.reply(data.cnt);  
-        }
-        if (!text) return citel.reply(`Hey there! ${citel.pushName}. How are you doing these days?`);
-        const { Configuration, OpenAIApi } = require("openai");
-        const configuration = new Configuration({
-            apiKey: Config.OPENAI_API_KEY || "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",
-        });
-        const openai = new OpenAIApi(configuration);
-        const completion = await openai.createCompletion({
-            model: "text-davinci-002",
-            prompt: text,
-            temperature: 0.5,
-            max_tokens: 80,
-            top_p: 1.0,
-            frequency_penalty: 0.5,
-            presence_penalty: 0.0,
-            stop: ['"""'],
-        });
-        citel.reply(completion.data.choices[0].text);
-    }
-)
-//---------------------------------------------------------------------------
-cmd({
-        pattern: "repo",
-        alias: ["git", "sc", "script"],
-        desc: "Sends info about repo.",
-        category: "general",
+        pattern: "settings",
+        alias: ["setting", "seettings", "setting menu"],
+        desc: "Sends info about settings",
+        category: "owner",
         filename: __filename,
     },
     async(Void, citel) => {
-        let { data } = await axios.get('https://api.github.com/repos/X-Notiya/-')
-        let cap = `𝐃𝚫𝚪𝐊 𝐐𝐔𝚵𝚵𝚴 𝐕1
-╔💓ʜᴇʏ..... ${citel.pushName}\n
-╚❄️ᴛᴏᴛᴀʟ ꜱᴛᴀʀᴛꜱ ${data.stargazers_count} ꜱᴛᴀʀꜱ
-╔🧚‍♂️ꜰᴏʀᴋꜱ ${data.forks_count} ꜰᴏʀᴋꜱ
-╚🍁ʀᴇᴘᴏ https://github.com/X-Notiya`
+        let { data } = await axios.get('https://github.com/X-Notiya/-)
+        let cap = `DarkQueen MD
+❍━SETTINGS━❍
+🧜‍♀️setvar THEME: eg: DARK QUEEN
+🧜‍♂️setvar OWNER_NAME eg: CHAMODH `
         let buttonMessaged = {
             image: { url: await botpic() },
             caption: cap,
@@ -67,7 +35,7 @@ cmd({
             headerType: 4,
             contextInfo: {
                 externalAdReply: {
-                    title: "Secktor-Repo",
+                    title: "ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴍᴅ",
                     body: "Easy to Use",
                     thumbnail: log0,
                     mediaType: 4,
@@ -84,45 +52,17 @@ cmd({
 )
 //---------------------------------------------------------------------------
 cmd({
-        pattern: "status",
-        alias: ["about"],
-        desc: "To check bot status",
-        category: "general",
-        filename: __filename,
-    },
-    async(Void, citel) => {
-        const uptime = process.uptime();
-        timestampe = speed();
-        latensie = speed() - timestampe;
-        let ter = `
- 💕${tlang().title} 💕
-🍁ꜱᴘᴇᴇᴅ..${latensie.toFixed(4)}
-❄️ᴜᴘᴛɪᴍᴇ..${runtime(process.uptime())}
-💓ᴠᴇʀꜱɪᴏɴ..1.0.0
-👤ᴏᴡɴᴇʀ..𝐜𝐡𝐚𝐦𝐨𝐝𝐡
-🎭ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀʀᴋ Qᴜᴇᴇɴ🎭
-`;
-        let buttonMessaged = {
-            image: {
-                url: await botpic(),
-            },
-            caption: ter,
-            footer: tlang().footer,
-            headerType: 4,
-            contextInfo: {
-                externalAdReply: {
-                    title: tlang().title,
-                    body: `Bot-Status`,
-                    thumbnail: log0,
-                    mediaType: 2,
-                    mediaUrl: ``,
-                    sourceUrl: ``,
-                },
-            },
-        };
-        return await Void.sendMessage(citel.chat, buttonMessaged, {
-            quoted: citel,
-        });
+    pattern: "theme",
+    desc: "To find all themes",
+    category: "general",
+    filename: __filename,
+},
+async(Void, citel,text,{isCreator}) => {
 
-    }
+if(!isCreator) return citel.reply(tlang().owner);
+let str="*All available themes in Secktor*"
+str+=`1. SECKTOR\n2. ADAM\n3. AYANOKOJI\n4. EDITH\n5. FRIDAY\n6. GENOS\n7. GIDEON\n8. GOKU\n9. LUFFY\n10. NARUTO\n11. NEZUKO\n12. PARKER\n13. ZEROTWO\n14. Eren Jeager(Coming Soon)\n\n these are the themes of Secktor Userbot.\_Reply ${prefix}setvar THEME:ZEROTWO`
+return citel.reply(str)
+    
+}
 )
