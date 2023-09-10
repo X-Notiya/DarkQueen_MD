85c0f53b290cf0eba4407419f7eb01a3df2f66bb
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-10 18:39:12 +0530

    💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓

--- commands/bot.js
+++ commands/bot.js
@@ -1,97 +1,25 @@
-/**🎭𝑫𝑨𝑹𝑲 𝑸𝑼𝑬𝑬𝑵 𝑴𝑫🎭
-🎩𝑪𝑹𝑬𝑨𝑻𝑬𝑫 𝑩𝒀 𝑪𝑯𝑨𝑴𝑶𝑫𝑯🎩* @version 0.0.1
- **/
-
-const os = require('os')
-const moment = require("moment-timezone")
-const fs = require("fs")
-const Config = require('../config')
-let { fancytext, tlang, tiny, runtime, formatp, botpic, prefix, sck1 } = require("../lib");
-const long = String.fromCharCode(8206)
-const readmore = long.repeat(4001)
-const Secktor = require('../lib/commands')
-
-    //---------------------------------------------------------------------------
-Secktor.cmd({
-            pattern: "bot cmd",
-            alias: ["bot"],
-            desc: "bot list",
-            category: "general",
-            react: "🎭",
-            filename: __filename
-        },
-        async(Void, citel, text) => {
-            const { commands } = require('../lib');
-            if (text.split(" ")[0]) {
-                let arr = [];
-                const cmd = commands.find((cmd) => cmd.pattern === (text.split(" ")[0].toLowerCase()))
-                if (!cmd) return await citel.reply("*❌No Such commands.*");
-                else arr.push(`*🍁Command:* ${cmd.pattern}`);
-                if (cmd.category) arr.push(`*🧩Category:* ${cmd.category}`);
-                if (cmd.alias) arr.push(`*🧩Alias:* ${cmd.alias}`);
-                if (cmd.desc) arr.push(`*🧩Description:* ${cmd.desc}`);
-                if (cmd.use) arr.push(`*〽️Usage:*\n \`\`\`${prefix}${cmd.pattern} ${cmd.use}\`\`\``);
-                return await citel.reply(arr.join('\n'));
-            } else {
-                const cmds = {}
-                commands.map(async(command, index) => {
-                    if (command.dontAddCommandList === false && command.pattern !== undefined) {
-                        if (!cmds[command.category]) cmds[command.category] = []
-                        cmds[command.category].push(command.pattern)
-                    }
-                })
-                const time = moment(moment())
-                    .format('HH:mm:ss')
-                moment.tz.setDefault('Asia/KOLKATA')
-                    .locale('id')
-                const date = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
-                let total = await sck1.countDocuments()
-                let str = `✦` + fancytext(Config.ownername.split(' ')[0], 58) + ` ✦\n`
-                str +=
-                    '```' + `❍━━━━━━━━━━❍
-BOT COMMANDS LIST
-❍━━━━━━━━━━❍
-` + '```'
-                for (const category in cmds) 
-                {
-                   str += `●━💓${tiny(darkqueen)}💓━●\n` ;
-                   if(text.toLowerCase() == category.toLowerCase()){ str = `●━💓${tiny(darkqueen)}💓━●\n` ;      
-                        for (const plugins of cmds[category]) { str += `💓⃢✦ ${fancytext(plugins,1)}\n` ; }
-                        str += `✦━━━━━━━━━━✦\n`  ;
-                        break ;
-                   }
-                   else { for (const plugins of cmds[category]) { str += `💓⃢✦ ${fancytext(plugins,1)}\n` ; }
-                         str += `✦━━━━━━━━━━✦\n`  ; 
-                   }
-  
-                }
-                str+= `🎩 *_ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴍᴅ_*🎩
-🎭 *_ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ ɴᴏᴛɪʏᴀ_*🎭 `
-                let buttonMessaged = {
-                    image: { url: await botpic() },
-                    caption: str
-                };
-                    return await Void.sendMessage(citel.chat, buttonMessaged);
-            }
-        }
-    )
-
-Secktor.cmd({
-    pattern: "doc",
-    desc: "to get extact name where that command is in repo.\nSo user can edit that.",
-    category: "general",
-    react: "✨",
-    filename: __filename
-},
-async(Void, citel, text) => {
- const { commands } = require('../lib');
- let arr = [];
-        const cmd = commands.find((cmd) => cmd.pattern === (text.split(" ")[0].toLowerCase()))
-        if (!cmd) return await citel.reply("*❌No Such commands.*");
-        else arr.push(`*🍁Command:* ${cmd.pattern}`);
-        if (cmd.category) arr.push(`*🧩Type:* ${cmd.category}`);
-        if(cmd.filename) arr.push(`✨FileName: ${cmd.filename}`)
-        return citel.reply(arr.join('\n'));
+/**
+🎭𝑫𝑨𝑹𝑲 𝑸𝑼𝑬𝑬𝑵 𝑴𝑫🎭
+🎩𝑪𝑹𝑬𝑨𝑻𝑬𝑫 𝑩𝒀 𝑪𝑯𝑨𝑴𝑶𝑫𝑯🎩
+**/
 
 
-})
+const { Module } = require("..lib/");
+const Jimp = require("jimp");
+
+DarkQueencmd({
+        pattern: "setpp",
+        alias :['plist'],
+        category: "info",
+        react: "💱"
+        desc: "Shows list of all externally installed modules",
+        filename: __filename
+    },
+    async(message,match,m) => {
+       if (!message.reply_message.image)
+          return await message.reply("*𝐑𝐞𝐩𝐥𝐲 𝐭𝐨 𝐬 𝐩𝐡𝐨𝐭𝐨*")
+       let buff = await m.quoted.download();
+       await message.setPP(message.user,buff);
+       return await message.reply("*𝐏𝐫𝐨𝐟𝐢𝐥𝐞 𝐏𝐢𝐜𝐭𝐮𝐫𝐞 𝐔𝐩𝐝𝐚𝐭𝐞𝐝*");
+      }
+    );
