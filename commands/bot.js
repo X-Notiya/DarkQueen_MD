f032b937ce41421f82e7b1fb9f0b1c383ecd72b7
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-10 19:00:59 +0530

    💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓

--- commands/bot.js
+++ commands/bot.js
@@ -1,23 +1,20 @@
-/**
+/*
 🎭𝑫𝑨𝑹𝑲 𝑸𝑼𝑬𝑬𝑵 𝑴𝑫🎭
 🎩𝑪𝑹𝑬𝑨𝑻𝑬𝑫 𝑩𝒀 𝑪𝑯𝑨𝑴𝑶𝑫𝑯🎩
-**/
-
-
-const { Module } = require("..lib/");
+*/
+const { Module } = require("..lib");
 const Jimp = require("jimp");
 
-DarkQueencmd({
+cmd({
         pattern: "setpp",
         alias :['plist'],
         category: "info",
-        react: "💱"
-        desc: "Shows list of all externally installed modules",
+        desc: "Set profile picture",
         filename: __filename
     },
     async(message,match,m) => {
        if (!message.reply_message.image)
-          return await message.reply("*𝐑𝐞𝐩𝐥𝐲 𝐭𝐨 𝐬 𝐩𝐡𝐨𝐭𝐨*")
+          return await message.reply("_Reply to a photo_")
        let buff = await m.quoted.download();
        await message.setPP(message.user,buff);
        return await message.reply("*𝐏𝐫𝐨𝐟𝐢𝐥𝐞 𝐏𝐢𝐜𝐭𝐮𝐫𝐞 𝐔𝐩𝐝𝐚𝐭𝐞𝐝*");
