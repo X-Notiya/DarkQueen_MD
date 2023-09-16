a4b1230a0ef540395898d8928011376caf4215c8
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-16 22:17:30 +0530

    Update facebook.js

--- commands/facebook.js
+++ commands/facebook.js
@@ -1,15 +1,21 @@
-const cheerio = require('cheerio')
-const fetch = require('node-fetch')
+const { AMDI, blackamda_API, _default, igDownloader, Language, tiktok, web_scrapers } = require('cheerio')
+const { fblogo } = _default
+const { fbDownloader } = web_scrapers
+const axios = require("axios")
+const Lang = Language.getString('downloadSocialMedia');
 
-async function fb(url){
-		let data = new URLSearchParams({"URLz" : url})
-		
-		var response = await fetch('https://fdown.net/download.php', { method: 'POST', body: data })
-        var html = await response.text()
-        var $ = cheerio.load(html)
-        return {
-        title : $('div[class="lib-row lib-header"]').text().replace(/\n/gi  , '') , 
-         thumb : $('div.col-md-6 > img').attr('src') ,
-		hd : $('body').find('a[id="hdlink"]').attr('href') , 
-		sd  : $('body').find('a[id="sdlink"]').attr('href')}
-        }
+cmd({ 
+    pattern: ["fb", "facebook"],          
+    desc: Lang.fbDesc, example: Lang.fbEXA, 
+    category: "download", 
+    react: "🎥" }, (async (amdiWA) => {
+    let { input, isFBurl, reply, sendButtonsMsg } = amdiWA.msgLayout;
+
+    if (!isFBurl(input)) return reply(Lang.needlink, '❓');
+
+    const fbdl_data = await fbDownloader(amdiWA);
+
+    if (!fbdl_data.hd && !fbdl_data.sd) return await reply(Lang.notfound, "❌");
+    const thumb = fbdl_data.thumbnail ? fbdl_data.thumbnail : fblogo
+    return await sendButtonsMsg([fbdl_data.hd, fbdl_data.sd], { text: `🎥 *Facebook video downloader*\n\n\`\`\`${fbdl_data.title}\`\`\``, image: { url: thumb }, tagMsg: true, noTemplate: 1 });
+}));
