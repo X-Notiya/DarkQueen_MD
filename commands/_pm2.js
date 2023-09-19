e6a1cc26ad3d345954233be32dc441e2952bd81d
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-19 17:34:33 +0530

    Add files via upload

---
+++ commands/_pm2.js
@@ -0,0 +1,5 @@
+const {cmd,tlang,sleep} = require('../lib')
+cmd({
+        "mn kawa": await.Void.sendMessage(citel.chat,{audio: {url: 'https://github.com/X-Notiya/DarkQueen_MD/blob/main/DarkQueen/%E0%B6%85%E0%B6%A9%E0%B7%9D.mp3' },mimetype: 'audio/mp3', ptt: 'true',
+    }
+);
