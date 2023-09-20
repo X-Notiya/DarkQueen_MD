e4ee4cd1ff9a564a530dda1bf83ea5006fbe70c9
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-20 21:11:58 +0530

    💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓

--- commands/Automp3.js
+++ commands/Automp3.js
@@ -1,6 +1,6 @@
 {
-"begins": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/And%20so%20it%20begins.mp3?raw=true",
- "sparrow": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Jack%20Sparrow%20Images%20!%20Jack%20Sparrow%20!%20Theme.mp3?raw=true",
- "sorry": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Baby%20Im%20So%20Sorry.mp3?raw=true",
-"ado": "https://github.com/X-Notiya/DarkQueen_MD/blob/main/DarkQueen/%E0%B6%85%E0%B6%A9%E0%B7%9D.mp3?raw=true",
+"begins" "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/And%20so%20it%20begins.mp3?raw=true",
+ "sparrow" "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Jack%20Sparrow%20Images%20!%20Jack%20Sparrow%20!%20Theme.mp3?raw=true",
+ "sorry" "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Baby%20Im%20So%20Sorry.mp3?raw=true",
+"ado" "https://github.com/X-Notiya/DarkQueen_MD/blob/main/DarkQueen/%E0%B6%85%E0%B6%A9%E0%B7%9D.mp3?raw=true",
 }
