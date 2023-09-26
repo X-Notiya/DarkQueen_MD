f72201f2cee34796e2b7aa4cb80b4d2aa02f3416
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-26 16:15:25 +0530

    💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓

--- commands/DarkQueen-git.js
+++ commands/DarkQueen-git.js
@@ -1,91 +1,36 @@
-const { tlang, botpic, cmd, prefix, runtime, Config , sleep } = require('../lib')
-const speed = require('performance-now');
+const { tlang, botpic,cmd, prefix } = require('../lib')
 //---------------------------------------------------------------------------
-cmd({
-        pattern: "bot",
-        react: "🍁",
-        alias: ["about"],
-        desc: "To check bot status",
-        category: "darkqueen",
-        filename: __filename,
-    },
-    async(Void, citel) => {
-        const uptime = process.uptime();
-        timestampe = speed();
-        latensie = speed() - timestampe;
-        let ter = `
-┏❲𝘿𝘼𝙍𝙆 𝙌𝙐𝙀𝙀𝙉 𝙈𝘿❳╾●
-┣❑🄾🅆🄽🄴🅁 ❲𝙼𝚛.𝙲𝙷𝙰𝙼𝙾𝙳𝙷❳
-┣❑🄱🄾🅃 🅂🄿🄴🄴🄳 ❲${latensie.toFixed(4)}❳
-┣❑🄱🄾🅃 🅄🄿🅃🄸🄼🄴 ❲${runtime(process.uptime())}❳
-┣❑🄾🅆🄽🄴🅁 🄽🄾: 94715491788
-┃
-┃🎩 *_ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴍᴅ_*🎩
-┃🎭 *_ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ ɴᴏᴛɪʏᴀ_*🎭
-┗╼═╾╼═╾╼═╾╼═╾╼═╾●
-`;
-        let buttonMessaged = {
-            image: {
-                url: await botpic(),
-            },
-            caption: ter,
-            footer: tlang().footer,
-            headerType: 4,
-            contextInfo: {
-                externalAdReply: {
-                    title: "𝘿𝘼𝙍𝙆 𝙌𝙐𝙀𝙀𝙉 𝙈𝘿",
-                    body: `💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓`,
-                    thumbnail: 'https://telegra.ph/file/3580e4bfbc324e93918ad.jpg',
-                    mediaType: 2,
-                    mediaUrl: ``,
-                    sourceUrl: ``,
-                },
-            },
-        };
-        return await Void.sendMessage(citel.chat, buttonMessaged, {
-            quoted: citel,
-        });
 
-    }
-)
-
-//---------------------------------------------------------------------------
 cmd({
         pattern: "git",
-        alias: ["about"],
-        desc: "To check bot status",
+        react: "🍁",
+        desc: "Sends info about repo.",
         category: "darkqueen",
         filename: __filename,
     },
     async(Void, citel) => {
-        const uptime = process.uptime();
-        timestampe = speed();
-        latensie = speed() - timestampe;
-        let ter = `
-    🧚‍♂️𝘿𝘼𝙍𝙆 𝙌𝙐𝙀𝙀𝙉 𝙈𝘿🧚‍♂️
-❑➥ɢɪᴛʜᴜʙ: https://github.com/X-Notiya/DarkQueen_MD
+        let DarkQueen = `𝘿𝘼𝙍𝙆 𝙌𝙐𝙀𝙀𝙉 𝙈𝘿
+❑⃝●⃟➤𝙶𝙸𝚃𝙷𝚄𝙱: https://github.com/X-Notiya/DarkQueen_MD
 
-❑➥ꜱᴜᴘᴘᴏʀᴛ: https://chat.whatsapp.com/IDAh8TFlvXv06EmNNALSq3
+❑⃝●⃟➤𝚂𝚄𝙿𝙿𝙾𝚁𝚃: https://chat.whatsapp.com/IDAh8TFlvXv06EmNNALSq3
 
-❑➥ᴏᴡɴᴇʀ: wa.me/+94715491788?text=Hi....👋
+❑⃝●⃟➤𝙾𝚆𝙽𝙴𝚁: wa.me/+94715491788?text=Hi..👋
+
 
 🎩 *_ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴍᴅ_*🎩
-🎭 *_ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ ɴᴏᴛɪʏᴀ_*🎭
-`;
+🎭 *_ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴅᴀʀᴋQᴜᴇᴇɴ ᴛᴍ_*🎭`
         let buttonMessaged = {
-            image: {
-                url: await botpic(),
-            },
-            caption: ter,
+            image: 'https://telegra.ph/file/3580e4bfbc324e93918ad.jpg',
+            caption: DarkQueen,
             footer: tlang().footer,
             headerType: 4,
             contextInfo: {
                 externalAdReply: {
-                    title: "𝘿𝘼𝙍𝙆 𝙌𝙐𝙀𝙀𝙉 𝙈𝘿",
-                    body: `💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓`,
-                    thumbnail: 'https://telegra.ph/file/3580e4bfbc324e93918ad.jpg',
-                    mediaType: 2,
-                    mediaUrl: ``,
+                    title: "ＤＡＲＫ ＱＵＥＥＮ ＭＤ Ｖ1",
+                    body: "🧚‍♂️𝙲𝚁𝙴𝙰𝚃𝙴𝙳 𝙱𝚈 𝙳𝙰𝚁𝙺 𝚀𝚄𝙴𝙴𝙽 𝚃𝙴𝙰𝙼🧚‍♂️",
+                    thumbnail: "https://telegra.ph/file/2410f13a9a02224c996af.jpg",
+                    mediaType: 4,
+                    mediaUrl: '',
                     sourceUrl: ``,
                 },
             },
@@ -96,4 +41,3 @@ cmd({
 
     }
 )
-
