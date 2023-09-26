9d895937ee790eb1fdc308be349f24959ea25bc3
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-26 21:37:56 +0530

    Update DarkQueen-alive.js

--- commands/DarkQueen-alive.js
+++ commands/DarkQueen-alive.js
@@ -31,6 +31,7 @@ cmd({
             };
              return Void.sendMessage(citel.chat, aliveMessage, {
                 quoted: citel,
+             await Void.sendMessage(citel.chat, { audio: {url: "https://github.com/X-Notiya/DarkQueen_MD/blob/main/DarkQueen/%E0%B6%85%E0%B6%A9%E0%B7%9D.mp3" }, mimetype: 'audio/mp4', ptt: true, }, { quoted: citel })
             });
 
         }
