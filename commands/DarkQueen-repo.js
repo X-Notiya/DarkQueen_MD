40c4449cc3c4794b90511f24f65efd01a35ced0e
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-26 15:51:18 +0530

    💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓

--- commands/DarkQueen-repo.js
+++ commands/DarkQueen-repo.js
@@ -1,86 +1,38 @@
-const { tlang, botpic, cmd, prefix, runtime, Config , sleep } = require('../lib')
-const axios = require('axios')
-const speed = require('performance-now')
-const fetch = require('node-fetch');
+const { addnote, cmd, sck1, delnote, allnotes, delallnote, tlang, botpic, runtime, prefix, sleep} = require('../lib') 
 //---------------------------------------------------------------------------
 cmd({
-        pattern: "alive",
-        desc: "Sends info about alive.",
-        category: "darkqueen",
-        filename: __filename
-    },
-    async(Void, citel) => {
-        let cap = `┏╼❲𝘿𝘼𝙍𝙆 𝙌𝙐𝙀𝙀𝙉 𝙈𝘿❳╼❋
-┃👋𝙷𝙴𝙻𝙻𝙾𝚆  ${citel.pushName}
-┃ ↱𝙸𝙰𝙼 𝙳𝙰𝚁𝙺 𝚀𝚄𝙴𝙴𝙽 𝙼𝙳↲
-┃❲𝚠𝚐𝚊𝚝𝚜𝚊𝚙𝚙 𝚞𝚜𝚎𝚛 𝚋𝚘𝚝❳
-┃
-┣❑𝙱𝙾𝚃 𝚂𝙿𝙴𝙴𝙳: ❲${latensie.toFixed(4)}❳
-┣❑𝙱𝙾𝚃 𝙽𝙰𝙼𝙴: ᴅᴀʀᴋ Qᴜᴇᴇɴ
-┣❑𝙱𝙾𝚃 𝙾𝚆𝙽𝙴𝚁: ᴍʀ.ᴄʜᴀᴍᴏᴅʜ
-┣❑𝙼𝙴𝙼𝙾𝚁𝚈
-┣❑𝙳𝙰𝚃𝙴
-┣❑𝚃𝙸𝙼𝙴
-┣❑𝙱𝙾𝚃 𝚄𝙿𝚃𝙸𝙼𝙴 ❲${runtime(process.uptime())}❳
+            pattern: "alive",
+            category: "general",
+            filename: __filename,
+            desc: "is bot alive??"
+        },
+        async(Void, citel, text, isAdmins) => {
+            let alivetext = `
+┏╼═╾╼═╾╼═╾╼═╾╼═╾❋
+┃ʜᴇʟʟᴏᴡ @user 
+┃ɪᴀᴍ ᴅᴀʀᴋ Qᴜᴇᴇɴ ʙᴏᴛ
+┣❑ᴏᴡɴᴇʀ: 𝙲𝙷𝙰𝙼𝙾𝙳𝙷
+┣❑ᴏᴡɴᴇʀ ɴᴏ: 94715491788
+┣❑ᴜᴘᴛɪᴍᴇ: ${runtime(process.uptime())}
+┣❑ᴠᴇʀꜱɪᴏɴ: 0.1
+┣❑ʙʀᴀɴᴄʜ: 𝙼𝙰𝙸𝙽
 ┃
 ┃🎩 *_ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴍᴅ_*🎩
 ┃🎭 *_ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ ɴᴏᴛɪʏᴀ_*🎭
-┗╼═╾╼═╾╼═╾╼═╾╼═╾❋`
-        let buttonMessaged = {
-            image: 'https://telegra.ph/file/2410f13a9a02224c996af.jpg',
-            caption: cap,
-            footer: tlang().footer,
-            headerType: 4,
-            contextInfo: {
-                externalAdReply: {
-                    title: "ＤＡＲＫ ＱＵＥＥＮ ＭＤ Ｖ1",
-                    body: "🍁ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴛᴇᴇᴍ🍁",
-                    thumbnail: 'https://telegra.ph/file/3580e4bfbc324e93918ad.jpg',
-                    mediaType: 4,
-                    mediaUrl: '',
-                    sourceUrl: `wa.me/+94715491788?text=hi.....👋`,
-                },
-            },
-        };
-        return await Void.sendMessage(citel.chat, buttonMessaged, {
-            quoted: citel,
-        });
-
-    }
-)
-//---------------------------------------------------------------------------
-cmd({
-        pattern: "botlist",
-        desc: "To check bot status",
-        category: "darkqueen",
-        filename: __filename
-    },
-    async(Void, citel) => {
-        let ter = `❋${tiny(category)}❋\n
-
-❋🧜‍♀️⃝►● ${fancytext(plugins,1)}\n
+┗╼═╾╼═╾╼═╾╼═╾╼═╾❋
 `;
-        let buttonMessaged = {
-            image: {
-                url: await botpic(),
-            },
-            caption: ter,
-            footer: tlang().footer,
-            headerType: 4,
-            contextInfo: {
-                externalAdReply: {
-                    title: "ＤＡＲＫ ＱＵＥＥＮ ＭＤ Ｖ1",
-                    body: `🍁ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴛᴇᴇᴍ🍁`,
-                    thumbnail: log0,
-                    mediaType: 2,
-                    mediaUrl: ``,
-                    sourceUrl: ``,
+            let aliveMessage = {
+                image: {
+                    url: await botpic(),
                 },
-            },
-        };
-        return await Void.sendMessage(citel.chat, buttonMessaged, {
-            quoted: citel,
-        });
+                caption: alivetext,
+                footer: tlang().footer,
+                headerType: 4,
+            };
+             return Void.sendMessage(citel.chat, aliveMessage, {
+                quoted: citel,
+            });
 
-    }
-)
+        }
+    )
+/**👋හායි.....?**/
