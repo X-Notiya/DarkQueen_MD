3ee420912452bea6419480f08d8c7e6652a472df
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-09-27 16:21:31 +0530

    💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓

--- commands/DarkQueen-fbdl.js
+++ commands/DarkQueen-fbdl.js
@@ -6,7 +6,7 @@ const {  fbdl1 } = require('vihangayt-fbdl')
 
 cmd({
             pattern: "fbdl",
-            desc: "Downloads fb videos  .",
+            desc: "Downloads fb videos.",
             category: "downloader",
             filename: __filename,
             use: '<add fb url.>'
@@ -17,16 +17,11 @@ cmd({
 
 	      let res = await fbdl1(!text)
           let vurl=res.url[0].url;
-
-          let data  ="*Video Name       :* "+ res.meta.title ;
-            	data +="\n*Video Duration : *" + res.meta.duration ;
-             	data +="\n*Video Link     :* "+  vurl;
-
-                        let buttonMessage = {
+              let buttonMessage = {
                         video: {url:vurl},
                         mimetype: 'video/mp4',
                         fileName: res.meta.title+`.mp4`,
-                        caption : "    *FACEBOOK DOWNLOADER*  \n"+data
+                        caption : "*FACEBOOK DOWNLOADER*/n "
                         
                     }
                  Void.sendMessage(citel.chat, buttonMessage, { quoted: citel });
