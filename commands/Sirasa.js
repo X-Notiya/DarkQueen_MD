c5bcb6af7571c7cc870a7b153f7b3fd975cf2397
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-10-01 21:39:08 +0530

    💓ᴅᴀʀᴋQᴜᴇᴇɴ•ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ.ɴᴏᴛɪʏᴀ💓

--- commands/Sirasa.js
+++ commands/Sirasa.js
@@ -1,1 +1,45 @@
-function _0x2cc1(_0x58f7e5,_0x189669){const _0xc84392=_0xc843();return _0x2cc1=function(_0x2cc130,_0x38dce8){_0x2cc130=_0x2cc130-0x171;let _0x1c75ea=_0xc84392[_0x2cc130];return _0x1c75ea;},_0x2cc1(_0x58f7e5,_0x189669);}const _0x5e994f=_0x2cc1;(function(_0x45a4dc,_0x740e61){const _0x2def9c=_0x2cc1,_0x4c6c71=_0x45a4dc();while(!![]){try{const _0x5f292f=parseInt(_0x2def9c(0x195))/0x1+-parseInt(_0x2def9c(0x18c))/0x2*(-parseInt(_0x2def9c(0x180))/0x3)+-parseInt(_0x2def9c(0x185))/0x4+-parseInt(_0x2def9c(0x18a))/0x5*(-parseInt(_0x2def9c(0x178))/0x6)+parseInt(_0x2def9c(0x17a))/0x7*(parseInt(_0x2def9c(0x188))/0x8)+-parseInt(_0x2def9c(0x184))/0x9+-parseInt(_0x2def9c(0x18f))/0xa*(parseInt(_0x2def9c(0x172))/0xb);if(_0x5f292f===_0x740e61)break;else _0x4c6c71['push'](_0x4c6c71['shift']());}catch(_0x4034f7){_0x4c6c71['push'](_0x4c6c71['shift']());}}}(_0xc843,0xb85f7));const _0x392d70=_0x5f15;function _0x5f15(_0x2336d3,_0x1ee372){const _0x5f329c=_0x2218();return _0x5f15=function(_0x52badc,_0x2b9347){_0x52badc=_0x52badc-0xe6;let _0x17af1d=_0x5f329c[_0x52badc];return _0x17af1d;},_0x5f15(_0x2336d3,_0x1ee372);}(function(_0xd4b32a,_0x2ec02c){const _0x5d0711=_0x2cc1,_0xa4972f=_0x5f15,_0x17bff4=_0xd4b32a();while(!![]){try{const _0x4a21d6=-parseInt(_0xa4972f(0xf8))/0x1*(-parseInt(_0xa4972f(0xef))/0x2)+-parseInt(_0xa4972f(0xf6))/0x3*(-parseInt(_0xa4972f(0xed))/0x4)+parseInt(_0xa4972f(0xf4))/0x5*(parseInt(_0xa4972f(0xf3))/0x6)+-parseInt(_0xa4972f(0xe7))/0x7*(parseInt(_0xa4972f(0xf2))/0x8)+parseInt(_0xa4972f(0xea))/0x9+-parseInt(_0xa4972f(0xfd))/0xa*(-parseInt(_0xa4972f(0xfe))/0xb)+-parseInt(_0xa4972f(0xf0))/0xc*(parseInt(_0xa4972f(0xf9))/0xd);if(_0x4a21d6===_0x2ec02c)break;else _0x17bff4[_0x5d0711(0x190)](_0x17bff4[_0x5d0711(0x189)]());}catch(_0x56ae16){_0x17bff4[_0x5d0711(0x190)](_0x17bff4[_0x5d0711(0x189)]());}}}(_0x2218,0x6e0ca));const {cmd}=require(_0x5e994f(0x18b)),sirasanews=require(_0x392d70(0xfa));function _0x2218(){const _0x556e92=_0x5e994f,_0x55f44d=[_0x556e92(0x179),_0x556e92(0x173),'192TrNcSP','link',_0x556e92(0x194),'372DSVzWx',_0x556e92(0x187),_0x556e92(0x191),_0x556e92(0x181),_0x556e92(0x17b),_0x556e92(0x17c),_0x556e92(0x171),'sirasa-news',_0x556e92(0x18e),'result','40lxAZBf',_0x556e92(0x182),_0x556e92(0x192),_0x556e92(0x193),'*_|:̵❑⃝💓⃟➥ꜱɪʀᴀꜱᴀ\x20ɴᴇᴡꜱ_*\x20\x0a\x20\x20\x20\x20\x0a❑⃝🍁⃟➥\x20ᴛɪᴛᴇʟ\x20:\x20\x20',_0x556e92(0x175),_0x556e92(0x17e),_0x556e92(0x17f),_0x556e92(0x17d),_0x556e92(0x18d)];return _0x2218=function(){return _0x55f44d;},_0x2218();}function _0xc843(){const _0x3a457a=['69610XAMhyn','8NOgvWv','shift','895dSGyZv','../lib','2625822FReIWf','387488qzbIbe','\x0a\x0a❑⃝🥀⃟➥\x20ᴅᴀᴛᴇ\x20&\x20ᴛɪᴍᴇ\x20:\x20','6780NtKAUF','push','title','sirasa','220367saXqet','104QipqWp','518950QxynDK','1833884PcKWqb','9856WjCEzO','44cELFyS','result','\x0a𝘿𝘼𝙍𝙆\x20𝙌𝙐𝙀𝙀𝙉\x20𝙉𝙀𝙒𝙎\x0a\x0a\x20©ᴅᴇᴠᴇʟᴏᴘᴇᴅ\x20ʙʏ\x20ᴍʀ.\x20ɴᴏᴛɪʏᴀ\x20','🧜‍♂️','darkqueen','28236gKBHQp','chat','9178246FuZwRx','dateandtime','39279SUxkiI','description','6057837qahTgw','\x0a\x0a❑⃝🧚‍♂️⃟➥\x20ᴜʀʟ\x20:\x20','3GUZHZH','18kbuqlr','372559mcHEbP','sendMessage','12514329ZtoyMJ','4929012jpOVmK','\x0a\x0a❑⃝🧜‍♀️⃟➥\x20ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ\x20:\x20'];_0xc843=function(){return _0x3a457a;};return _0xc843();}cmd({'pattern':_0x392d70(0xe6),'category':_0x5e994f(0x177),'react':_0x5e994f(0x176),'filename':__filename},async(_0x50182b,_0x30b278,_0x369259)=>{const _0x54b9ed=_0x5e994f,_0x5a27da=_0x392d70,_0xe9a8bb=await sirasanews(),_0x57e0ef=_0x5a27da(0xe8)+_0xe9a8bb['result'][_0x5a27da(0xf5)]+_0x5a27da(0xfb)+_0xe9a8bb[_0x5a27da(0xfc)][_0x5a27da(0xf7)]+_0x54b9ed(0x186)+_0xe9a8bb[_0x5a27da(0xfc)][_0x5a27da(0xec)]+_0x5a27da(0xeb)+_0xe9a8bb[_0x5a27da(0xfc)][_0x5a27da(0xf1)]+_0x5a27da(0xe9);await _0x50182b[_0x54b9ed(0x183)](_0x30b278[_0x5a27da(0xee)],{'image':{'url':_0xe9a8bb[_0x54b9ed(0x174)]['image']},'caption':_0x57e0ef},{'quoted':_0x30b278});});
+const { tlang, botpic, cmd, prefix, } = require('../lib')
+const axios = require('axios')
+
+//---------------------------------------------------------------------------
+cmd({
+        pattern: "සිරස",
+        alias: ["sirasanewd", "sn", "sirasa news"],
+        desc: "Sends info about repo.",
+        category: "general",
+        react: "✅",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        let { data } = await axios.get('https://gist.githubusercontent.com/X-Notiya/a7d8a39e82fc93c800d1043c200f3144/raw/a52e01c01a4e6ce0c1656a0d9aa0e9eff2a54f6a/DarkQueen-Sirasanews.txt')
+        let cap = `
+ᴛɪᴛʟᴇ: ${title}
+ᴅᴀᴛᴇ ᴀɴ ᴛɪᴍᴇ ${dateandtime}
+ʟɪɴᴋ: ${link}
+ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ: ${description}
+
+🎩 *_ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴍᴅ_*🎩
+🎭 *_ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴛᴇᴀᴍ_*🎭
+`
+        let buttonMessaged = {
+            image: { url: ${image} },
+            caption: cap,
+            footer: tlang().footer,
+            headerType: 4,
+            contextInfo: {
+                externalAdReply: {
+                    title: "DARK QUEEN SIRASA NEWS",
+                    body: "© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴛᴇᴀᴍ",
+                    thumbnail: log0,
+                    mediaType: 4,
+                    mediaUrl: '',
+                    sourceUrl: ``,
+                },
+            },
+        };
+        return await Void.sendMessage(citel.chat, buttonMessaged, {
+            quoted: citel,
+        });
+
+    }
+)
